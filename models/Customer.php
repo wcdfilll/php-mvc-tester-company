0c1be5b0e7bb82339b79483a5d9d2cfa462f7287
nhatCreA <wcdfill@gmail.com>
2025-01-03 08:28:55 +0700

    model for Customer.php

---
+++ models/Customer.php
@@ -0,0 +1,15 @@
+<?php
+class Customer {
+    public $id;
+    public $name;
+    public $email;
+    public $address;
+}
+
+public function __construct($id, $name, $email, $address){
+    $this->id = $id;
+    $this->name = $name;
+    $this->email = $email;
+    $this->address = $address;
+}
+?>
